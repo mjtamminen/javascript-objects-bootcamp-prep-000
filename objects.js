66993e7db48832850994b88055c531df3b66c8e2
tamminen.mikkoj@gmail.com <tamminen.mikkoj@gmail.com>
2018-09-14 08:20:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
-  return playlist;
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist
 }
